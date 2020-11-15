14bf4c0594863e85b9059c396577a5dd02cb6fc1
dynadermel <72702819+dynadermel@users.noreply.github.com>
2020-11-15 18:34:37 +0100

    Update formulaire.js

--- tp2-aw/formulaire.js
+++ tp2-aw/formulaire.js
@@ -1,1 +1,31 @@
+function validation(){
+  
+ 
+  
+ 
+  document.getElementById("error").innerHTML = "";
+  
+for(let item of document.querySelectorAll('.inputAW')){
+  if(item.value.length < 5){
+    // recuperation du label
+    var label =   document.querySelector("label[for="+item.id+"]").textContent;
+    // ajout du message d'erreur
+    document.getElementById("error").innerHTML += `Minimum 5 caractères pour : ${label} <br/>`;
+    
+   
+    document.getElementById("error").classList.add("display");
+      document.getElementById("resultat").classList.remove("display")
 
+  }
+    
+}
+  if(document.getElementById("error").innerHTML == ""){
+    var nom = document.getElementById("nom").value
+    document.getElementById("resultat").innerHTML = `Bienvenue à ${nom}`;
+    document.getElementById("error").classList.remove("display")
+  document.getElementById("resultat").classList.add("display")
+  }
+   
+
+  
+}
