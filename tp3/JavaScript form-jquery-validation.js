7e958847f4bb10ce73275d031302e6f96f3fe30b
dynadermel <72702819+dynadermel@users.noreply.github.com>
2020-11-27 18:11:28 +0100

    Add files via upload

---
+++ tp3/JavaScript form-jquery-validation.js
@@ -0,0 +1,41 @@
+
+
+
+   $(document).ready(function () {
+ 
+   
+    $("#button").click(function (event) {
+
+        event.preventDefault();
+
+    
+        if (($("#nom").val() && $("#prénom").val() && $("#date").val() && $("#adresse").val() && $("#mail").val()) != "") {
+
+
+            $('#myModal').modal({ show: true })
+            $(".modal-title").text("Bienvenue " + $("prénom").val());
+
+            var city = $("#adresse").val();
+            var date = $("#date").val();
+
+            $(".modal-body").html(`Vous êtes né(e) ${date} et vous habitez à :
+            <a class="link"  href="http://maps.google.com/maps?" ><img src="https://maps.googleapis.com/maps/api/staticmap?markers=${city}&zoom=14&size=400x300&scale=2&key=AIzaSyAkmvI9DazzG9p77IShsz_Di7-5Qn7zkcg"/> Lien Google Maps </a>`);
+
+        } else {
+
+            $('#myModal').modal({ show: true })
+            $(".modal-body").html('Veuillez renseigner tous les champs s\'il vous plait');
+
+        }
+    });
+
+    
+   
+
+
+});
+    
+
+
+            
+   
